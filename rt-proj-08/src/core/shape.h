295f2007b730cc908886bdb723c09b49ef462574
André Freitas <freitasandre38@gmail.com>
2024-06-21 11:15:48 -0300

    feat: add missing attributes to shape.h

--- rt-proj-08/src/core/shape.h
+++ rt-proj-08/src/core/shape.h
@@ -17,6 +17,11 @@ public:
 
     virtual bool intersect_p(const Ray &r, real_type maxT ) const = 0;
     virtual bool intersect(const Ray &r, std::shared_ptr<Surfel> &isect) const = 0;
+
+    // This really should be here?
+    const Transform * obj_to_world; //!< Object to World transformation (scene specified).
+    const Transform * world_to_obj; //!< World to Object trasformation (deduced base on O2W).
+    const bool flip_normals;
 };
 
 } // namespace rt3
