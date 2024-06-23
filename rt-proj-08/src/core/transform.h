9e56ddc939b380e6cab255ed570b6ff4a70f390b
André Freitas <freitasandre38@gmail.com>
2024-06-23 08:26:16 -0300

    refactor: remake transform .h class

--- rt-proj-08/src/core/transform.h
+++ rt-proj-08/src/core/transform.h
@@ -1,30 +1,41 @@
 #ifndef TRANSFORM_H
 #define TRANSFORM_H
 
+#include "rt3.h"
+#include "ray.h"
+#include "vector3f.h"
+#include <memory>
+
 namespace rt3{
 
 class Transform {
 public:
-    Transform(const float m[4][4]) {};
+    Transform();
+    Transform(const float m[4][4]);
+    Transform(const  Matrix4x4 &m);
+    Transform(const  Matrix4x4 &m, const Matrix4x4 &mInv);
     ~Transform() = default;
 
-    friend Transform Inverse(const Transform &t) {};
-    friend Transform Transpose(const Transform &t) {};
+    friend Transform Inverse(const Transform &t);
+    friend Transform Transpose(const Transform &t);
 
-    Transform Translate(const Vector3f &delta){};
-    Transform Scale(Float x, Float y, Float z) {};
-    Transform RotateX(Float theta) {};
-    Transform Rotate(Float theta, const Vector3f &axis) {};
+    Transform Translate(const Vector3f &delta) const;
+    Transform Scale(float x, float y, float z) const;
+    Transform RotateX(float theta);
+    Transform Rotate(float theta, const Vector3f &axis);
+
+    //Idk if this really will be useful
+    bool HasScale() const; // defined in pbr-book
+    bool IsIdentity() const;
+    bool operator==(const Transform &t) const;
+    bool operator!=(const Transform &t) const;
+
+private:
+    Matrix4x4 m, mInv;
+
 };
 
 } // namespace rt3
-
-
-
-
-
-
-
 
 
 
